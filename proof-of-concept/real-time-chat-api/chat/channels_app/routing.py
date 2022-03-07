00bed70fd096d5b46481c3193a452974e9e9eb55
sam-cowing <sam.ryan.cowing@gmail.com>
2022-03-07 17:29:24 -0500

    minor poc updates

--- proof-of-concept/real-time-chat-api/chat/channels_app/routing.py
+++ proof-of-concept/real-time-chat-api/chat/channels_app/routing.py
@@ -4,7 +4,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/chat$', consumers.ChatConsumer),
 ]
 
 application = ProtocolTypeRouter({
