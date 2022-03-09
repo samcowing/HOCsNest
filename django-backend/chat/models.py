94acc891d396f9a300119c02a92dad75e3825231
david-c-oliveros <david3oliveros@gmail.com>
2022-03-09 17:24:48 -0500

    merge changes

--- django-backend/chat/models.py
+++ django-backend/chat/models.py
@@ -11,4 +11,3 @@ class Message(models.Model):
     #TODO - implement User sessions and auth
     message = models.TextField()
     user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
