8dd7c2976168e372316330e0af6128d98236a629
joshhovis <joshuahovis3@gmail.com>
2022-03-15 13:12:50 -0400

    Changing color scheme for light/dark themes

--- react-frontend/src/theme.js
+++ react-frontend/src/theme.js
@@ -1,32 +1,33 @@
 export const lightTheme = {
     body: '#fff',
     primaryText: 'rgba(0, 0, 0, 0.87)',
-    secondaryText: '#fb8c00',
+    secondaryText: '#00C8E7',
+    activeUserText: '#009F59',
 
-    cardTop: '#ffcc80',
+    cardTop: '#93CDA5',
     cardTopHover: '#fff',
     cardTopActive: '#fff',
     cardTitle: '#fff',
-    cardTitleHover: '#ffcc80',
-    cardTitleActive: '#ffcc80',
+    cardTitleHover: '#93CDA5',
+    cardTitleActive: '#93CDA5',
 
-    userIcon: '#ffcc80',
-    activeUserIcon: '#03ccc97a',
-
-    activeUserText: '#00B0AE',
+    userIcon: '#BEFBFF',
+    activeUserIcon: '#93CDA5',
 
     inputBackground: 'rgba(81, 82, 85, 0.25)',
     sendBtn: '#00B0AE',
-    sendBtnHover: '#ffe0b2',
+    sendBtnHover: '#D3FADE',
 
-    themeBtn: '#ffe0b2',
-    themeBtnHover: '#03ccc97a',
+    themeBtn: '#00C8E7',
+    themeBtnHover: '#93CDA5',
 }
 
 export const darkTheme = {
     body: '#121212',
+
     primaryText: '#fff',
-    secondaryText: '#7e57c2',
+    secondaryText: '#4F7FD1',
+    activeUserText: '#7e57c2',
 
     cardTop: '#9575cd',
     cardTopHover: '#fff',
@@ -35,15 +36,13 @@ export const darkTheme = {
     cardTitleHover: '#9575cd',
     cardTitleActive: '#9575cd',
 
-    userIcon: '#9575cd',
-    activeUserIcon: '#588eeb',
-
-    activeUserText: '#4F7FD1',
+    userIcon: '#588eeb',
+    activeUserIcon: '#9575cd',
 
     inputBackground: 'rgba(81, 82, 85, 0.25)',
     sendBtn: '#4F7FD1',
     sendBtnHover: '#c0b0df',
 
-    themeBtn: '#9575cd',
-    themeBtnHover: '#588eeb',
+    themeBtn: '#588eeb',
+    themeBtnHover: '#9575cd',
 }
