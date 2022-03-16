879dcdd2b503d1eec2fe8c2891c63a9da2c21107
joshhovis <joshuahovis3@gmail.com>
2022-03-16 07:59:22 -0400

    Resolving errors and django dependency install

--- react-frontend/src/components/Chat/Chat.jsx
+++ react-frontend/src/components/Chat/Chat.jsx
@@ -163,7 +163,7 @@ function Chat({ theme }) {
         <div className='chat'>
           <div className='chat-map'>
             {messages.map(message =>
-              <div key={message.username} className={'chat-map-wrapper' + (message.username === user.username ? ' active-user' : '')} >
+              <div className={'chat-map-wrapper' + (message.username === user.username ? ' active-user' : '')} >
                 <Avatar src='https://i.imgur.com/W7mI5kZ.png' alt={user.username} className={'chat-map-avatar' + (message.username === user.username ? ' active-user-avatar' : '')} />
                 <div className='chat-map-userinput'>
                   <h5 className={'chat-map-user' + (message.username === user.username ? ' active-user-text' : '')}>{message.username}</h5>
@@ -200,8 +200,8 @@ function Chat({ theme }) {
                 }} />
             }
             <button className='btn chat-form-btn' type='submit'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
               </svg>
             </button>
           </form>
